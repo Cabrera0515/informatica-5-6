0865c7bd56bafcaa3beddc4a58d54cf116bde2b7
Cabrera0515 <z744s2tk8g@privaterelay.appleid.com>
2026-09-01 16:36:39 +0000

    at

---
+++ weekly 3/atmosphere.py
@@ -0,0 +1,28 @@
+def main():
+    layer1 = input("Descent atmosphere layer: ")
+
+    if layer1 == "Exosphere":
+        print("Your altitude layer would be between 700 and 10,000 km")
+    elif layer1 == "Thermosphere":
+        print("Your altitude layer would be between 85 and 700 km")
+    elif layer1 == "Mesosphere":
+        print("Your altitude layer would be between 50 and 85 km")
+    elif layer1 == "Stratosphere":
+        print("Your altitude layer would be between 12 and 50 km")
+    elif layer1 == "Troposphere":
+         print("Your altitude layer would be between 0 and 12 km")
+    else:
+         print("Not valid option")
+
+    print(input("Enter exact altitud: "))
+    altitud1 = "Thermosphere":
+    if altitud = >85<700:
+        result = (altitud - 50) + (35/0.2) + (38/0.075) + (12/0.02)
+        print(result)
+
+
+
+
+
+if __name__ == "__main__":
+        main()
