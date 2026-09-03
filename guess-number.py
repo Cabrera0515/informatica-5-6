9053824ae76072aaf6a79f2c05e0015776942d8b
Cabrera0515 <z744s2tk8g@privaterelay.appleid.com>
2026-09-03 17:28:30 +0000

    past

---
+++ guess-number.py
@@ -0,0 +1,15 @@
+import random
+
+def main():
+    Name = input("Hello! What is your name? ")
+    print("medium level")
+    print("Easy 1-20")
+    print("Medium 1-100")
+    print("hard 1-100000")
+
+    level = input("Choose a difficulty level: ").strip().lower()
+    if level == ("Easy"):
+        print("level selected", level):
+        guess
+    print("I am thinking of a number between 1 and 100.")
+    print("Take a guess")
