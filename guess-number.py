b4271a5a87ec2abeb3bc4a65aada39e611157f1e
Cabrera0515 <z744s2tk8g@privaterelay.appleid.com>
2026-09-03 20:17:46 +0000

    commit

--- guess-number.py
+++ guess-number.py
@@ -1,15 +1,30 @@
 import random
 
 def main():
-    Name = input("Hello! What is your name? ")
+    Name = input("Hello! What is your name?")
+    print("Well, {name}, I am thinking of a number between 1 and 100.")
     print("medium level")
-    print("Easy 1-20")
-    print("Medium 1-100")
+    print("Easy 1- 10")
+    print("Medium 1-1000")
     print("hard 1-100000")
 
     level = input("Choose a difficulty level: ").strip().lower()
     if level == ("Easy"):
-        print("level selected", level):
-        guess
+        print("level selected", level)
+        guess = random.randint(
+            print("Well,-, name, "I am thinking of a number in between 1 and 10.")
+
+     if level == ("medium"):
+        print("You selected level", level)
+        guess = random.randint(1, 1000)
+        print("Well,-, name, "I am thinking of a number in between 1 and 1000.")
+
+     if level == ("Hard"):
+        print("You selected level!, level)
+              guess = random.randint(1, 100000)
+
+
+
+        )
     print("I am thinking of a number between 1 and 100.")
     print("Take a guess")
