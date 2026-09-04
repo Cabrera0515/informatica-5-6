74c9dfe66ba6bb124fe909d482e1ffa94dfc8977
Cabrera0515 <z744s2tk8g@privaterelay.appleid.com>
2026-09-04 17:26:05 +0000

    s

---
+++ space-weight.py
@@ -0,0 +1,21 @@
+def main():
+    Name = input("Hello! What is your name?")
+        print("Well, {triny}, i wanna know my weight on this planet.")
+         if layer1 == "mercury":
+                print("Your weight will be 0.38 ")
+            elif layer1 == "venus":
+                print("Your weight will be 0.91  ")
+            elif layer1 == "mars":
+                print("Your weight will be 0.38")
+            elif layer1 == "jupiter":
+                print("Your weight will be 2.53")
+            elif layer1 == "saturn":
+                 print("Your weight will be 1.07 ")
+            else:
+                 print("Not valid option")
+
+            print(input("Enter exact altitud: "))
+            altitud1 = "Thermosphere":
+            if altitud = >85<700:
+                result = (altitud - 50) + (35/0.2) + (38/0.075) + (12/0.02)
+                print(result)
